c24d2a4da3bc5a99ac6d8c3673b412587df3c3a4
guithepc <guilherme.pc@yahoo.com>
2021-09-13 22:25:22 -0300

    finalizando mocks

--- src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
+++ src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
@@ -86,7 +86,7 @@ class FinalizarLeilaoServiceTest {
 		}
 		
 		
-		
+		 
 		
 	}
 	
--- src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
+++ src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
@@ -1,11 +1,16 @@
 package br.com.alura.leilao.service;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -13,6 +18,7 @@ import org.mockito.MockitoAnnotations;
 import br.com.alura.leilao.dao.PagamentoDao;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Pagamento;
 import br.com.alura.leilao.model.Usuario;
 
 class GeradorDePagamentoTest {
@@ -23,19 +29,41 @@ class GeradorDePagamentoTest {
 	@Mock
 	private PagamentoDao pagamentoDao;
 	
+	@Captor
+	private ArgumentCaptor<Pagamento> captor;
+	
+	@Mock
+	private Clock clock;
+	
 	@BeforeEach
 	public void beforeEach() {
 		MockitoAnnotations.initMocks(this);
-		this.gerador = new GeradorDePagamento(pagamentoDao);
+		this.gerador = new GeradorDePagamento(pagamentoDao, clock);
 	}
 	
 	@Test
 	void deveriaCriarPagamentoVencedor() {
 		Leilao leilao = leilao();
 		Lance vencedor = leilao.getLanceVencedor();
+		
+		LocalDate dataFixa = LocalDate.of(2020, 12, 7);
+ 		
+		Instant instant = dataFixa.atStartOfDay(ZoneId.systemDefault()).toInstant();
+		
+		Mockito.when(clock.instant()).thenReturn(instant);
+		Mockito.when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+		
 		gerador.gerarPagamento(vencedor);
 		
-		Mockito.verify(pagamentoDao).salvar(pagamento);
+		Mockito.verify(pagamentoDao).salvar(captor.capture());
+		
+		Pagamento pagamento = captor.getValue();
+		
+		Assertions.assertEquals(dataFixa, pagamento.getVencimento());
+		Assertions.assertEquals(vencedor.getValor(), pagamento.getValor());
+		Assertions.assertFalse(pagamento.getPago());
+		Assertions.assertEquals(vencedor.getUsuario(), pagamento.getUsuario());
+		Assertions.assertEquals(leilao, pagamento.getLeilao());
 	}
 
 	private Leilao leilao(){
@@ -47,6 +75,7 @@ class GeradorDePagamentoTest {
 		leilao.propoe(primeiroLance);
 		leilao.setLanceVencedor(primeiroLance);
 		return leilao;
+		
 	}
 	
 }
